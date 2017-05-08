e1540f412fcc1b3a548026cb3ec78ddb7dc83d00
ProjectNorn <darkisvoid@gmail.com>
2017-05-07 20:08:31 -0400

    Update room_client.py

--- roompi/room_client.py
+++ roompi/room_client.py
@@ -22,7 +22,6 @@ import datetime
 import pickle
 import logging
 # from notifications import *
-import pygame
 from gtts import gTTS
 
 # If modifying these scopes, delete your previously saved credentials
@@ -91,6 +90,11 @@ server.connect()
 
 # Install signal handler--------------------------------------------------
 def exitclean():
+    l = clients.keys()
+    for k in l:
+        del clients[k]
+    print("clean up!")
+    time.sleep(1)
     GPIO.cleanup()
     quit()
 
@@ -105,7 +109,7 @@ GPIO.setup(13, GPIO.OUT)
 def clienthandler(uid):
     userinfo = server.get_user_info(uid)
     if userinfo is None:
-       #  flashRedLED()
+        flashRedLED()
         audio_notification('Please register first', 'please_register.mp3')
         logging.info('{} tries to log in but has not registered yet'.format(uid))
         return
@@ -115,10 +119,12 @@ def clienthandler(uid):
         user_credentials = pickle.loads(userinfo['credentials'])
     except KeyError:
         print("Key error while getting user credentials")
+        GPIO.cleanup()
         exit()
     except Exception as e:
         print(type(e))
         print("Error while getting user credentials")
+        GPIO.cleanup()
         exit()
 
 
@@ -126,27 +132,28 @@ def clienthandler(uid):
     events = getEvents(user_credentials)
     while uid in clients:
         if events:
-            print
             logging.info("{}'s event has been collected".format(uid))
             for i in range(len(events)):
                 e = datetime.datetime.strptime(events[i]['start'].get('dateTime', events[i]['start'].get('date'))[:-6], '%Y-%m-%dT%H:%M:%S')
                 events[i] = (e, events[i]['summary'])
-            print(events)
             now = datetime.datetime.now()
-            if (now+datetime.timedelta(minutes=15)) >= events[0][0]:
-                t = datetime.datetime.now().minute - events[0][0].minute
-                logging.info("Notify {} event in {} min".format(uid, t))
-                audio_notification("Attention, you have an event about {} in {} minutes".format(events[0][1], t), uid)
-                while datetime.datetime.now() < (now + datetime.timedelta(seconds=15)):
-                    flashLEDs()
+            for item in events:
+                if now.minute - item.minute > 0:
+                    events.remove(item)
 
-            elif (now+datetime.timedelta(minutes=5)) >= events[0][0]:
-                t = datetime.datetime.now().minute - events[0][0].minute
+            if (now+datetime.timedelta(minutes=5)) >= events[0][0]:
+                t = events[0][0].minute - now.minute
                 audio_notification("Attention, you have an event about {} in {} minutes".format(events[0][1], t), uid)
                 logging.info("Notify {} event in 5 min".format(uid))
                 while datetime.datetime.now() < (now + datetime.timedelta(seconds=15)):
                     flashLEDs()
-
+            elif (now+datetime.timedelta(minutes=15)) >= events[0][0]:
+                t = events[0][0].minute - now.minute
+                logging.info("Notify {} event in {} min".format(uid, t))
+                audio_notification("Attention, you have an event about {} in {} minutes".format(events[0][1], t), uid)
+                while datetime.datetime.now() < (now + datetime.timedelta(seconds=15)):
+                    flashLEDs()                    
+        
         time.sleep(120)
         events = getEvents(user_credentials)
         logging.info("Updating {}'s event list".format(uid))
@@ -159,19 +166,13 @@ while True:
     # Scan for cards
     (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-    # If a card is found
-    # if status == MIFAREReader.MI_OK:
-        # print("Card detected")
 
     # Get the UID of the card
     (status, ruid) = MIFAREReader.MFRC522_Anticoll()
 
     # If we have the UID, continue
     if status == MIFAREReader.MI_OK:
-        # print("UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[2]))
-        # GPIO.output(15, GPIO.HIGH)
-        # time.sleep(0.5)
-        # GPIO.output(15, GPIO.LOW)
+
         uid = '.'.join(str(v) for v in ruid).strip()
         if uid not in clients:
             clients[uid] = threading.Thread(name=uid, target=clienthandler, args=(uid, ))
